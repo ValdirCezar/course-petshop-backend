af16d3562789ae88ad783862a2a01745825b0f96
Valdir Cezar <valdir_jesus@sicredi.com.br>
2022-07-02 10:44:15 -0300

    chore: start project

---
+++ src/main/java/br/com/valdircezar/petshop/PetShopBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopBackendApplication.class, args);
+	}
+
+}
