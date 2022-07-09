d23a90371f8437412276def34ad6757c36de2361
Valdir Cezar <valdir_jesus@sicredi.com.br>
2022-07-09 16:12:45 -0300

    feat: add exception handler to validation exception

--- src/main/java/br/com/valdircezar/petshop/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/br/com/valdircezar/petshop/controller/exception/ControllerExceptionHandler.java
@@ -2,12 +2,14 @@ package br.com.valdircezar.petshop.controller.exception;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
 import static java.time.LocalDateTime.now;
+import static java.util.Objects.requireNonNull;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice
@@ -16,7 +18,19 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
                                                                          HttpServletRequest request) {
-        final var error = new StandardError(BAD_REQUEST.value(), BAD_REQUEST.getReasonPhrase(), now(), ex.getMessage(), request.getRequestURI());
-        return ResponseEntity.status(BAD_REQUEST).body(error);
+                return ResponseEntity.badRequest().body(
+                        new StandardError(BAD_REQUEST.value(),
+                                BAD_REQUEST.getReasonPhrase(), now(), ex.getMessage(), request.getRequestURI())
+                );
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationException(MethodArgumentNotValidException ex,
+                                                                         HttpServletRequest request) {
+        return ResponseEntity.badRequest().body(
+                new StandardError(BAD_REQUEST.value(), "Validation error", now(),
+                        requireNonNull(ex.getBindingResult().getFieldError().getDefaultMessage()), request.getRequestURI())
+        );
+    }
+    
 }
